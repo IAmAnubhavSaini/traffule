f50906ac17ce5da8956e838894f6690c58a6a492
Anubhav Saini <IAmAnubhavSaini@gmail.com>
2016-04-09 22:45:18 +0530

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name traffuleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the traffuleApp
+ */
+angular.module('traffuleApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name traffuleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the traffuleApp
+ */
+angular.module('traffuleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
