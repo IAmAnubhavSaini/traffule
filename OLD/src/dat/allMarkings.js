fa8cc921a573a4ae8ca32c2567d76a9f9b5f9e05
Anubhav <iamanubhavsaini@gmail.com>
2021-05-05 22:57:09 +0530

    OLD

---
+++ OLD/src/dat/allMarkings.js
@@ -0,0 +1,39 @@
+﻿{
+  "markings": [
+    {
+      "markingName": "SingleSolidWhiteLine"
+      , "markingDescription": "Single solid white line on a 2 way road separates oncoming and going traffic. <br />Solid line is not meant to be crossed or straddled with. Keep clear of solid white line."
+      , "markingCaption": "Single broken white line."
+    },
+    {
+      "markingName": "DoubleSolidWhiteLine"
+      , "markingDescription": ""
+      , "markingCaption": "Double solid white line."
+    },
+    {
+      "markingName": "SingleBrokenWhiteLine"
+      , "markingDescription": "Single broken white line on a 2 way road separates oncoming and going traffic. <br />Dashed/broken line can be crossed to overtake if safe."
+      , "markingCaption": "Single broken white line."
+    },
+    {
+      "markingName": "MixedSolidBrokenWhiteLine"
+      , "markingDescription": "Mixed solid broken white lines on a 2 way road separates oncoming and going traffic. <br />If you are driving on broken/dashed line side, you can cross the central lines to overtake if safe; otherwise if solid line is on your side then you can not cross or overtake in any case."
+      , "markingCaption": "Mixed solid broken white line"
+    },
+    {
+      "markingName": "MixedBrokenSolidWhiteLine"
+      , "markingDescription": "Mixed broken solid white lines on a 2 way road separates oncoming and going traffic. <br />If you are driving on broken/dashed line side, you can cross the central lines to overtake if safe; otherwise if solid line is on your side then you can not cross or overtake in any case."
+      , "markingCaption": "Mixed broken solid white line"
+    },
+    {
+      "markingName": "SingleSolidWhite4Lane"
+      , "markingDescription": "Single central solid line separates 2 way traffic, for four lanes of traffic. Solid line is not meant to be crossed or straddled with."
+      , "markingCaption": "4 lane central single solid white line"
+    },
+    {
+      "markingName": "SingleBrokenWhite4Lane"
+      , "markingDescription": "Single central broken line separates 2 way traffic, for four lanes of traffic. Broken central line can be crossed for overtaking if safe."
+      , "markingCaption": "4 lane central single broken white line"
+    }
+  ]
+}
