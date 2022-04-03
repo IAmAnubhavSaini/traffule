8965b7800695a3bfd57167c2754c16beb37f707b
f0c1s <iamanubhavsaini+git@gmail.com>
2022-04-03 09:14:02 +0530

    Removing old

--- OLD/gh-pages/README.md
+++
@@ -1,2 +0,0 @@
-# traffule
-TrafficRules related application.
--- OLD/src/res/scripts/executeWidgets.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('#tt').TopTaker({ 
-    'theme': 'dark',
-    'text': ' back to top',
-    'iconClass': 'icon-circle-arrow-up icon-white '
-  });
-});
